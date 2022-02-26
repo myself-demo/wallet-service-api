ca879b62462ca275ffca548721675a8941f55ea9
Husky <husky.robot.dog@gmail.com>
2022-02-26 15:23:50 +0800

    init

---
+++ src/main/java/com/github/myself_demo/wallet_service_api/Main.java
@@ -0,0 +1,11 @@
+package com.github.myself_demo.wallet_service_api;
+
+import com.github.myself_demo.wallet_service_api.service.WalletServiceProvider;
+
+public class Main {
+    public static void main(String[] args) {
+        WalletServiceProvider walletServiceProvider = new WalletServiceProvider();
+
+    }
+}
+
